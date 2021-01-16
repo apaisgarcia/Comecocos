d1e735141057da4bcfdd2143ca7d05557d608075
apaisgarcia <aliapaisgarcia@gmail.com>
2021-01-16 16:43:09 +0100

    movimiento pacman

---
+++ script/pacman.js
@@ -0,0 +1,38 @@
+const speed=32;
+class Pacman extends Gameobject
+{
+    constructor(x,y){
+        super(x,y);
+
+        this.lives=3; //esto son las vidas
+        this.score=0; //son las vidas
+
+    }
+
+//keypresed es la funcion para mover con la flecha
+    showInstanceMode(p, imgPacman) {
+        p.image(imgPacman,this.coordX,this.coordY);
+    }
+//keypresed es la funcion para mover con la flecha
+
+    moveRight() {
+
+            this.coordX = this.coordX + speed;
+            console.log("avanzo derecha", this.coordX);
+            //  this.direction=1;// pongo d
+
+
+    }
+    moveLeft() {
+
+        this.coordX = this.coordX - speed;
+        console.log("avanzo izquierda", this.coordX);
+
+    }
+    moveDown() { //mover abajo
+        this.coordY=this.coordY+speed;
+    }
+    moveUp(){ //mover arriba
+        this.coordY=this.coordY-speed;
+    }
+}
--- script/sketch.js
+++ script/sketch.js
@@ -1,48 +1,44 @@
 
 
-var imgPacman;
+
 const s = (p) => {
     var imgRoca;
     var imgComida;
+    var imgPacman;
     var arrayRocasMapa = [];
-    var arrayComidaMapa= []; // esto es como un new array
+    var arrayComidaMapa = []; // esto es como un new array
     var myJuego = new Game(); // si hago yo el array
     var myJuego2 = new Game(18, 25); //aray aleatorio
+    var myPacman = new Pacman(32, 64);
 
-
-     p.preload = function() { // cargar antes de comenzar img = loadImage("images/pacman32.png");k
+    p.preload = function () { // cargar antes de comenzar img = loadImage("images/pacman32.png");k
         imgPacman = p.loadImage("images/pacman32.png");
+        //console.log(imgPacman);
+
         imgRoca = p.loadImage("images/roca.jpg");
-        imgComida= p.loadImage("images/comida.jpg")
+        imgComida = p.loadImage("images/comida.jpg")
     }
 
-    p.setup = function() { //crear la pantalla
-
-       p.createCanvas(COLUMNS * SIZE_IMAGE, ROWS * SIZE_IMAGE);
-       // p.createCanvas(myJuego.columnGame*myJuego.sizeImage, myJuego.rowsGame*myJuego.sizeImage); // ancho y alto (el orden)
-       // console.log("Filas :", myJuego.mapa.length);
+    p.setup = function () { //crear la pantalla
+        console.log("estoy en draw");
+        p.createCanvas(COLUMNS * SIZE_IMAGE, ROWS * SIZE_IMAGE);
+        // p.createCanvas(myJuego.columnGame*myJuego.sizeImage, myJuego.rowsGame*myJuego.sizeImage); // ancho y alto (el orden)
+        // console.log("Filas :", myJuego.mapa.length);
 
         for (let i = 0; i < myJuego.mapa.length; i++) { //entro en i y j
             for (let j = 0; j < myJuego.mapa[i].length; j++) {
 
-               /*
-                     Puede ser de la forma de arriba o esta....
-
-                    for (let i = 0; i < myJuego.mapa.length; i++) { //entro en i y j
-                    for (let j = 0; j < myJuego.mapa[i].length; j++) {
-               */
-
                 if (myJuego.mapa[i][j] === 1) {
-                  //  console.log("Añado roca en fila, ", i);
-                   // console.log("Añado roca en columna , ", j);
-                    arrayRocasMapa.push(new Roca(myJuego.sizeImage * j, myJuego.sizeImage * i)); //
+                    //  console.log("Añado roca en fila, ", i);
+                    // console.log("Añado roca en columna , ", j);
+                    arrayRocasMapa.push(new Roca(myJuego.sizeImage * j, myJuego.sizeImage * i));
 
                 } else {
-                   // console.log("No hay roca");
+                    // console.log("No hay roca");
                 }
-             if (myJuego.mapa[i][j] === 0) {
+                if (myJuego.mapa[i][j] === 0) {
 
-                    arrayComidaMapa.push(new Comida(myJuego.sizeImage * j, myJuego.sizeImage * i)); //
+                    arrayComidaMapa.push(new Comida(myJuego.sizeImage * j, myJuego.sizeImage * i));
 
                 } else {
                     // console.log("No hay comida");
@@ -50,24 +46,49 @@ const s = (p) => {
 
             }//cierro j
         }// cierro i
+        // p.image(imgPacman,myPacman.coordX,myPacman.coordY); NO HACE  FALTA
 
 
     }
 
 
-    p.draw = function() { //
+    p.draw = function () { //
+
+
         // clear(); // le quita el rastro
         p.background(15);
-      //  console.log("estoy en draw");
+       // console.log("estoy en draw");
+
         for (let i = 0; i < arrayRocasMapa.length; i++) {
 
-            arrayRocasMapa[i].showinstanceMode(p,imgRoca);
+            arrayRocasMapa[i].showinstanceMode(p, imgRoca);
         }
-       for (let s = 0; s < arrayComidaMapa.length; s++) {
+        for (let s = 0; s < arrayComidaMapa.length; s++) {
 
-            arrayComidaMapa[s].showinstanceMode(p,imgComida);
+            arrayComidaMapa[s].showinstanceMode(p, imgComida);
+        }
+        myPacman.showInstanceMode(p, imgPacman);
+    }//CIERRE DRAW
+
+    p.keyPressed = function () {
+        console.log("Presionado");
+        if (p.keyCode === p.RIGHT_ARROW) {
+            myPacman.moveRight();
+            console.log("flecha drch presionada");
+        }
+        if (p.keyCode === p.LEFT_ARROW) {
+            myPacman.moveLeft();
+            console.log("flecha izq presionada");
+        }
+        if (p.keyCode === p.DOWN_ARROW) {
+            myPacman.moveDown();
+        }
+        if (p.keyCode === p.UP_ARROW) {
+            myPacman.moveUp();
         }
 
     }
+
 }
+
 var myp5 = new p5(s,'myContainer');
