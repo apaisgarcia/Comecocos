2419c4fc4d4769c536c34d34da7640f4cd3c9e4a
apaisgarcia <aliapaisgarcia@gmail.com>
2021-02-21 20:59:59 +0100

    Información sobre el autor

---
+++ script/autor.js
@@ -0,0 +1,11 @@
+function autor() {
+
+    alert(
+        " Autora: Alia Pais 2021 " + "\n" +
+        " Idioma Juego: Castellano /Ingles" + "\n" +
+        " Información sobre el Código en GitHub : https://apaisgarcia.github.io/Comecocos/ " + "\n" +
+        " Realizado para : M6- Desarrollo Web Entorno Cliente " + "\n"
+
+    );
+
+}
